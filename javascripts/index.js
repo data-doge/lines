8122d51ca542d486566b700b9917ac28cb942f81
euglazer <eugenestapler@gmail.com>
2015-05-09 18:11:38 +1200

    javascript to jquery

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
-	var main = document.getElementById('main');
-	var one = document.getElementById('one');
-	var two = document.getElementById('two');
-	var three = document.getElementById('three');
-	var four = document.getElementById('four');
-	var five = document.getElementById('five');
+	var $main = $('#main');
+	var $one = $('#one');
+	var $two = $('#two');
+	var $three = $('#three');
+	var $four = $('#four');
+	var $five = $('#five');
 	var ones = [];
 	var twos = [];
 	var threes = [];
@@ -12,30 +12,39 @@ $(document).ready(function () {
 	var fives = [];
 
 	function line() {
-		var line = document.createElement('div');
-		line.className = 'line';
-		return line;
+		var $line = $("<div class='line'></div>");
+		return $line;
 	}
-	function red(line,i) {
-		line.style.background = "rgba(242,56,90," + i / 255 + ")";
-		return line;
+	function red($line,i) {
+		$line.css({
+			backgroundColor : "rgba(242,56,90," + i / 255 + ")"
+		});
+		return $line;
 	}
-	function orange(line,i) {
-		line.style.background = "rgba(245,165,3," + i / 255 + ")";
-		return line;
+	function orange($line,i) {
+		$line.css({
+			backgroundColor : "rgba(245,165,3," + i / 255 + ")"
+		});
+		return $line;
 	}
-	function cream(line,i) {
-		line.style.background = "rgba(233,241,223," + i / 255 + ")";
-		return line;
+	function cream($line,i) {
+		$line.css({
+			backgroundColor : "rgba(233,241,223," + i / 255 + ")"
+		});
+		return $line;
 	}
-	function cyan(line,i) {
-		line.style.background = "rgba(74,217,217," + i / 255 + ")";
-		return line;
+	function cyan($line,i) {
+		$line.css({
+			backgroundColor : "rgba(74,217,217," + i / 255 + ")"
+		});
+		return $line;
 	}
 
-	function blue(line,i) {
-		line.style.background = "rgba(54,177,191," + i / 255 + ")";
-		return line;
+	function blue($line,i) {
+		$line.css({
+			backgroundColor : "rgba(54,177,191," + i / 255 + ")"
+		});
+		return $line;
 	}
 
 	function initColumns() {
@@ -49,11 +58,11 @@ $(document).ready(function () {
 	}
 
 	function clearScreen() {
-		one.innerHTML = '';
-		two.innerHTML = '';
-		three.innerHTML = '';
-		four.innerHTML = '';
-		five.innerHTML = '';
+		$one.html('');
+		$two.html('');
+		$three.html('');
+		$four.html('');
+		$five.html('');
 	}
 
 	function randChar() {
@@ -63,28 +72,27 @@ $(document).ready(function () {
 	function printLines() {
 		clearScreen();
 		for (var i = 0; i < 255; i++) {
-			one.appendChild(ones[i]);
-			two.appendChild(twos[i]);
-			three.appendChild(threes[i]);
-			four.appendChild(fours[i]);
-			five.appendChild(fives[i]);
+			$one.append(ones[i]);
+			$two.append(twos[i]);
+			$three.append(threes[i]);
+			$four.append(fours[i]);
+			$five.append(fives[i]);
 		}
-
-		var counter = document.createElement('p');
-		counter.innerText = randChar();
-		one.appendChild(counter);
-		var counter = document.createElement('p');
-		counter.innerText = randChar();
-		two.appendChild(counter);
-		var counter = document.createElement('p');
-		counter.innerText = randChar();
-		three.appendChild(counter);
-		var counter = document.createElement('p');
-		counter.innerText = randChar();
-		four.appendChild(counter);
-		var counter = document.createElement('p');
-		counter.innerText = randChar();
-		five.appendChild(counter);
+		var $counter = $('<p></p>');
+		$counter.text(randChar());
+		$one.append($counter);
+		var $counter = $('<p></p>');
+		$counter.text(randChar());
+		$two.append($counter);
+		var $counter = $('<p></p>');
+		$counter.text(randChar());
+		$three.append($counter);
+		var $counter = $('<p></p>');
+		$counter.text(randChar());
+		$four.append($counter);
+		var $counter = $('<p></p>');
+		$counter.text(randChar());
+		$five.append($counter);
 	}
 
 	function shiftLines(lines,times) {
@@ -96,12 +104,12 @@ $(document).ready(function () {
 
 	initColumns();
 
-	document.body.onkeyup = function(e) {
+	$(document).on('keyup', function (e) {
 		clearInterval(getWavey);
-		document.getElementsByTagName('header')[0].innerText = '';
-		document.getElementsByTagName('h2')[0].innerText = 'do it again';
+		$('header').text('');
+		$('h2').text('do it again');
 		var multiplier = Math.random() * 10;
-		if(e.keyCode == 13) {
+		if(e.keyCode === 13) {
 			oneStep = Math.ceil((Math.random() * multiplier));
 			twoStep = Math.ceil((Math.random() * multiplier));
 			threeStep = Math.ceil((Math.random() * multiplier));
@@ -115,6 +123,7 @@ $(document).ready(function () {
 			shiftLines(threes,threeStep);
 			shiftLines(fours,fourStep);
 			shiftLines(fives,fiveStep);
-		},20);
-	}
+		},100);
+	});
+
 });
