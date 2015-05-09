03f6fc180dc9fb3b2c43409f936d612539e895a1
euglazer <eugenestapler@gmail.com>
2015-05-10 11:20:01 +1200

    add load screen and format kotare

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,53 +1,29 @@
+var loadInterval;
+
 $(document).ready(function () {
+	var deg = 0;
+	loadInterval = setInterval(function () {
+		$('#load img').css({
+			transform: 'rotateY(' + deg % 360 + 'deg)'
+		});
+		deg += 5;
+	}, 20);
+});
 
-	var colors = [
-		{r: 242, g: 56, b: 90},
-		{r: 245, g: 165, b: 3},
-		{r: 233, g: 241, b: 223},
-		{r: 74, g: 217, b: 217},
-		{r: 54, g: 177, b: 191}
-	]
+$(window).load(function () {
 
-	var initialized = false;
-	var waves = []
-
-	function initializeWaves () {
-		for (var i = 0; i < colors.length; i++) {
-			$('header').hide();
-			$('section').show();
-			$('footer').show();
-			initialized = true;
-			var wave = new Wave(colors[i].r, colors[i].g, colors[i].b)
-			wave.initializeLines();
-			wave.renderLines();
-			waves.push(wave);
-		}
-	}
-
-	function resetRotation () {
-		for (var i = 0; i < waves.length; i++) {
-			waves[i].rotate();
-		}
-	}
-
-	function initializeChars () {
-		for (var i = 0; i < waves.length; i++) {
-			$('#characters').append("<div class='character'></div>");
-		}
-		var charInterval = setInterval(function () {
-			$('.character').each(function (i, character) {
-				$(character).text(randChar());
-			});
-		}, 20);
-	}
+	clearInterval(loadInterval);
+	$('#load').remove();
+	$('header').fadeIn(3000);
+	var ocean = new Ocean();
 
 	$(document).on('keyup', function (e) {
 		if (e.keyCode === 13) {
-			if (!initialized) {
-				initializeWaves();
-				initializeChars();
+			if (!ocean.initialized) {
+				ocean.initializeWaves();
+				ocean.initializeChars();
 			}
-			resetRotation();
+			ocean.resetRotation();
 		}
 	});
 
