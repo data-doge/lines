da10706a3bae846b7119ed95dc0e0d83b815fdee
euglazer <eugenestapler@gmail.com>
2015-05-09 19:48:08 +1200

    waves refactored and working

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,129 +1,40 @@
 $(document).ready(function () {
-	var $main = $('#main');
-	var $one = $('#one');
-	var $two = $('#two');
-	var $three = $('#three');
-	var $four = $('#four');
-	var $five = $('#five');
-	var ones = [];
-	var twos = [];
-	var threes = [];
-	var fours = [];
-	var fives = [];
 
-	function line() {
-		var $line = $("<div class='line'></div>");
-		return $line;
-	}
-	function red($line,i) {
-		$line.css({
-			backgroundColor : "rgba(242,56,90," + i / 255 + ")"
-		});
-		return $line;
-	}
-	function orange($line,i) {
-		$line.css({
-			backgroundColor : "rgba(245,165,3," + i / 255 + ")"
-		});
-		return $line;
-	}
-	function cream($line,i) {
-		$line.css({
-			backgroundColor : "rgba(233,241,223," + i / 255 + ")"
-		});
-		return $line;
-	}
-	function cyan($line,i) {
-		$line.css({
-			backgroundColor : "rgba(74,217,217," + i / 255 + ")"
-		});
-		return $line;
-	}
+	var colors = [
+		{r: 242, g: 56, b: 90},
+		{r: 245, g: 165, b: 3},
+		{r: 233, g: 241, b: 223},
+		{r: 74, g: 217, b: 217},
+		{r: 54, g: 177, b: 191}
+	]
 
-	function blue($line,i) {
-		$line.css({
-			backgroundColor : "rgba(54,177,191," + i / 255 + ")"
-		});
-		return $line;
-	}
+	var initialized = false;
+	var waves = []
 
-	function initColumns() {
-		for (var i = 0; i < 255; i++) {
-			ones.push(red(line(),i));
-			twos.push(orange(line(),i));
-			threes.push(cream(line(),i));
-			fours.push(cyan(line(),i));
-			fives.push(blue(line(),i));
+	function initializeWaves () {
+		for (var i = 0; i < colors.length; i++) {
+			$('header').hide();
+			$('#main').show();
+			initialized = true;
+			var wave = new Wave(colors[i].r, colors[i].g, colors[i].b)
+			wave.initializeLines();
+			wave.renderLines();
+			waves.push(wave);
 		}
 	}
 
-	function clearScreen() {
-		$one.html('');
-		$two.html('');
-		$three.html('');
-		$four.html('');
-		$five.html('');
-	}
-
-	function randChar() {
-		return String.fromCharCode(3e4+Math.random()*33);
-	}
-
-	function printLines() {
-		clearScreen();
-		for (var i = 0; i < 255; i++) {
-			$one.append(ones[i]);
-			$two.append(twos[i]);
-			$three.append(threes[i]);
-			$four.append(fours[i]);
-			$five.append(fives[i]);
-		}
-		var $counter = $('<p></p>');
-		$counter.text(randChar());
-		$one.append($counter);
-		var $counter = $('<p></p>');
-		$counter.text(randChar());
-		$two.append($counter);
-		var $counter = $('<p></p>');
-		$counter.text(randChar());
-		$three.append($counter);
-		var $counter = $('<p></p>');
-		$counter.text(randChar());
-		$four.append($counter);
-		var $counter = $('<p></p>');
-		$counter.text(randChar());
-		$five.append($counter);
-	}
-
-	function shiftLines(lines,times) {
-		for (var i = 0; i < times; i++) {
-			t = lines.shift();
-			lines.push(t);
+	function resetRotation () {
+		for (var i = 0; i < waves.length; i++) {
+			waves[i].rotate();
 		}
 	}
-
-	initColumns();
 
 	$(document).on('keyup', function (e) {
-		clearInterval(getWavey);
-		$('header').text('');
-		$('h2').text('do it again');
-		var multiplier = Math.random() * 10;
-		if(e.keyCode === 13) {
-			oneStep = Math.ceil((Math.random() * multiplier));
-			twoStep = Math.ceil((Math.random() * multiplier));
-			threeStep = Math.ceil((Math.random() * multiplier));
-			fourStep = Math.ceil((Math.random() * multiplier));
-			fiveStep = Math.ceil((Math.random() * multiplier));
+		if (e.keyCode === 13) {
+			if (!initialized) {
+				initializeWaves();
+			}
+			resetRotation();
 		}
-		var getWavey = setInterval(function() {
-			printLines();
-			shiftLines(ones,oneStep);
-			shiftLines(twos,twoStep);
-			shiftLines(threes,threeStep);
-			shiftLines(fours,fourStep);
-			shiftLines(fives,fiveStep);
-		},100);
 	});
-
 });
